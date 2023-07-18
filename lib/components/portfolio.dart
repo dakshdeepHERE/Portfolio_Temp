94543eb4bd50e2d8fb011afacd2e0b898eae78fc
Daksha Deep <dakshadeep1234@gmail.com>
2023-07-18 22:30:59 +0530

    added padding for the widgets and trying to fix color

---
+++ lib/components/portfolio.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Portfolio extends StatefulWidget {
+  const Portfolio({super.key});
+
+  @override
+  State<Portfolio> createState() => _PortfolioState();
+}
+
+class _PortfolioState extends State<Portfolio> {
+  List<Widget> navItems = [
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(onPressed: () {}, child: const Text('Education')),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(onPressed: () {}, child: const Text('Skills')),
+    ),
+  ];
+  bool isMobile = false;
+  @override
+  Widget build(BuildContext context) {
+    isMobile = MediaQuery.of(context).size.width > 700 ? false : true;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Daksha Deep'),
+        actions: isMobile ? null : navItems,
+      ),
+      drawer: isMobile
+          ? Drawer(
+              child: ListView(
+                children: navItems,
+              ),
+            )
+          : null,
+      body: Center(
+        child: Text('Portfolio'),
+      ),
+    );
+  }
+}
